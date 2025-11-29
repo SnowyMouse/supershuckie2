f9ae290160c0ee57d0d3275c2c9b59ae3e9d8c7d
Snowy <snowy@snowymouse.com>
2025-11-29 09:47:12 +0100

    Should use std::env instead of cfg for build.rs
    build.rs is compiled for the host. As such, cfg! won't actually work for
    us if we're cross-compiling. We need to use env variables, instead (in
    this case, CARG_CFG_TARGET_OS).

--- supershuckie-pokeabyte-integration/build.rs
+++ supershuckie-pokeabyte-integration/build.rs
@@ -1,11 +1,13 @@
 fn main() {
-    build_shared_memory_impl();
+    let target_os = std::env::var("CARGO_CFG_TARGET_OS").expect("no target OS set");
+    match target_os.as_str() {
+        "macos" => build_shared_memory_macos(),
+        unknown => unimplemented!("Poke-A-Byte integration for target_os '{unknown}' is not implemented")
+    }
 }
 
-#[cfg(target_os = "macos")]
-fn build_shared_memory_impl() {
+fn build_shared_memory_macos() {
     let mut build = cc::Build::new();
-
     build.file("src/shared_memory/macos.c");
-    build.compile("shared_memory_macos");
+    build.compile("pokeabyte_integration_shared_memory_macos");
 }
