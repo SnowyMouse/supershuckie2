f012af465bfd110e6b8d6bfcd48ae659fb03b960
Snowy <snowy@snowymouse.com>
2025-11-30 09:57:22 +0100

    WIP: Allow local moltenvk install

--- supershuckie/src/main.rs
+++ supershuckie/src/main.rs
@@ -1,6 +1,10 @@
+mod renderer;
+
 use sdl3_main::{app_impl, AppResult};
 use sdl3_sys::events::{SDL_Event, SDL_EventType};
 use std::sync::Mutex;
+use sdl3_sys::everything::SDL_WINDOW_VULKAN;
+use sdl3_sys::video::SDL_CreateWindow;
 
 #[derive(Default)]
 struct SuperShuckie {
@@ -9,10 +13,24 @@ struct SuperShuckie {
 #[app_impl]
 impl SuperShuckie {
     fn app_init() -> Option<Box<Mutex<SuperShuckie>>> {
+        let window = unsafe {
+            SDL_CreateWindow(
+                c"Super Shuckie 2.0 (name TBD)".as_ptr(),
+                640,
+                480,
+                SDL_WINDOW_VULKAN
+            )
+        };
+
+        if window.is_null() {
+            return None
+        }
+
         Some(Box::new(Mutex::new(SuperShuckie::default())))
     }
 
     fn app_iterate(&mut self) -> AppResult {
+
         AppResult::Continue
     }
 
